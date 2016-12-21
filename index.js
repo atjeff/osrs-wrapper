b872f5da260071ead1f8062486d887180992788e
Jeff Hage <Jeff Hage>
2016-12-21 17:57:29 -0500

    graphData

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const request = require('request');
 let ge = new GrandExchange(1);
 
 
-ge.itemInfo(2)
-    .then((item) => {
-        console.log(item);
+/*ge.itemInfo(2)
+    .then((itemData) => {
+        console.log(itemData);
+    });
+*/
+/*ge.graphData(2)
+    .then((graph) => {
+        console.log(graph);
+    });*/
+
+ge.categoryPrices(0,'a',1).then((graph) => {
+        console.log(graph);
     });
