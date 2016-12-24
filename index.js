af2a3ac2dea0bd94974fa8df63ee39803afd0c23
Jeff Hage <Jeff Hage>
2016-12-23 23:11:23 -0500

    Update folder structure, helper functions, added hiscores for single user

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 "use strict";
-const GrandExchange = require('./lib/api/ge');
+const GrandExchange = require('./lib/ge/ge');
+const Hiscores = require('./lib/hiscores/hiscores');
 const request = require('request');
-let ge = new GrandExchange(1);
 
 
-/*ge.itemInfo(2)
+let ge = new GrandExchange();
+let hiscores = new Hiscores();
+
+hiscores.getPlayer("buy tayrocs", "hardcore")
     .then((itemData) => {
         console.log(itemData);
     });
-*/
+
+
+/*ge.itemInfo("Bronze Bar")
+    .then((itemData) => {
+        console.log(itemData);
+    });*/
+
 /*ge.graphData(2)
     .then((graph) => {
         console.log(graph);
     });*/
-
-ge.categoryPrices(0,'a',1).then((graph) => {
-        console.log(graph);
-    });
